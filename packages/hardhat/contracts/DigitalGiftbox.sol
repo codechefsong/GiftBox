d852cf1ac76f16a746dd49741ca5f63914d40cf8
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2025-02-26 21:52:41 -0500

    add contracts

---
+++ packages/hardhat/contracts/DigitalGiftbox.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract DigitalGiftbox {
+    address creator;
+    string recipient;
+    string occasion;
+    string title;
+    uint256 createdAt;
+    uint256 deliveryDate;
+    bool isDelivered;
+    uint256 tokenPot;
+    string[] messages;
+    address[] contributors;
+
+    mapping(address => uint256[]) public createdGiftboxes;
+    mapping(address => uint256[]) public receivedGiftboxes;
+
+    event GiftboxCreated(address indexed creator, string recipient, uint256 deliveryDate);
+    event MessageAdded(address indexed contributor, string message);
+    event ContributionAdded(address indexed contributor, uint256 amount);
+    event GiftboxDelivered(address indexed recipient);
+
+    constructor(
+        string memory _recipient,
+        string memory _occasion,
+        string memory _title
+    ) {
+        creator = msg.sender;
+        recipient = _recipient;
+        occasion = _occasion;
+        title = _title;
+        createdAt = block.timestamp;
+    }
+
+    /**
+     * @dev Adds a message to a giftbox
+     * @param message The message to add
+     */
+    function addMessage(string memory message) public {
+        messages.push(message);
+        
+        bool isContributor = false;
+        for (uint i = 0; i < contributors.length; i++) {
+            if (contributors[i] == msg.sender) {
+                isContributor = true;
+                break;
+            }
+        }
+        
+        if (!isContributor) {
+            contributors.push(msg.sender);
+        }
+        
+        emit MessageAdded(msg.sender, message);
+    }
+
+    /**
+     * @dev Adds a contribution to a giftbox
+     */
+    function addContribution() public payable {
+        require(msg.value > 0, "Contribution must be greater than 0");
+        
+        tokenPot += msg.value;
+        
+        bool isContributor = false;
+        for (uint i = 0; i < contributors.length; i++) {
+            if (contributors[i] == msg.sender) {
+                isContributor = true;
+                break;
+            }
+        }
+        
+        if (!isContributor) {
+           contributors.push(msg.sender);
+        }
+        
+        emit ContributionAdded(msg.sender, msg.value);
+    }
+
+    
+    /**
+     * @dev Get all messages for a giftbox
+     */
+    function getGiftboxMessages() public view returns (string[] memory) {
+        return messages;
+    }
+
+    /**
+     * @dev Get all contributors for a giftbox
+     */
+    function getGiftboxContributors() public view returns (address[] memory) {
+        return contributors;
+    }
+
+    /**
+     * @dev Get giftbox details
+     * @param tokenId The ID of the giftbox
+     */
+    function getGiftboxDetails(uint256 tokenId) public view returns (
+        address _creator,
+        string memory _recipient,
+        string memory _occasion,
+        string memory _title,
+        uint256 _createdAt,
+        uint256 _deliveryDate,
+        bool _isDelivered,
+        uint256 _tokenPot,
+        uint256 _messageCount,
+        uint256 _contributorCount
+    ) { 
+        return (
+            creator,
+            recipient,
+            occasion,
+            title,
+            createdAt,
+            deliveryDate,
+            isDelivered,
+            tokenPot,
+            messages.length,
+            contributors.length
+        );
+    }
+}
---
+++ packages/hardhat/contracts/DigitalGiftboxFactory.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./DigitalGiftbox.sol";
+
+/**
+ * @title DigitalGiftboxFactory
+ * @dev A factory contract for creating new DigitalGiftbox contracts
+ */
+contract DigitalGiftboxFactory {
+    address[] public deployedGiftboxes;
+    mapping(address => address[]) public userGiftboxes;
+
+    event GiftboxCreated(address indexed giftboxAddress, address indexed creator, string title);
+    event ImplementationUpdated(address indexed oldImplementation, address indexed newImplementation);
+
+    constructor() {}
+
+    /**
+     * @dev Creates a new DigitalGiftbox contract
+     * @param recipient Name of the recipient
+     * @param occasion The occasion for the gift
+     * @param title The title of the giftbox
+     * @return giftboxAddress Address of the newly created DigitalGiftbox contract
+     */
+    function createGiftbox(
+        string calldata recipient,
+        string calldata occasion,
+        string calldata title
+    ) public returns (address giftboxAddress) {
+        DigitalGiftbox newDigitalGiftbox = new DigitalGiftbox(recipient, occasion, title);
+
+        deployedGiftboxes.push(address(newDigitalGiftbox));
+        userGiftboxes[msg.sender].push(address(newDigitalGiftbox));
+        
+        emit GiftboxCreated(giftboxAddress, msg.sender, title);
+        
+        return giftboxAddress;
+    }
+
+    /**
+     * @dev Get all giftboxes created by a specific user
+     * @param user Address of the user
+     * @return Array of giftbox contract addresses created by the user
+     */
+    function getUserGiftboxes(address user) external view returns (address[] memory) {
+        return userGiftboxes[user];
+    }
+
+    /**
+     * @dev Get total number of deployed giftboxes
+     * @return Total number of deployed giftboxes
+     */
+    function getTotalGiftboxCount() external view returns (uint256) {
+        return deployedGiftboxes.length;
+    }
+
+    /**
+     * @dev Get deployed giftboxes with pagination
+     * @param startIndex Start index for pagination
+     * @param count Number of items to return
+     * @return Array of giftbox addresses
+     */
+    function getDeployedGiftboxes(uint256 startIndex, uint256 count) external view returns (address[] memory) {
+        require(startIndex < deployedGiftboxes.length, "Start index out of bounds");
+        
+        if (startIndex + count > deployedGiftboxes.length) {
+            count = deployedGiftboxes.length - startIndex;
+        }
+        
+        address[] memory result = new address[](count);
+        
+        for (uint256 i = 0; i < count; i++) {
+            result[i] = deployedGiftboxes[startIndex + i];
+        }
+        
+        return result;
+    }
+}
